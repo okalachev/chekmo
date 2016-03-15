939517ce0a430b675c3bc867317f3b27459b39c4
Oleg Kalachev <okalachev@gmail.com>
2016-03-15 20:48:35 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,55 @@
+CHEKMO-II
+=========
+Binary + UCI adapter
+--------------------
+
+This is the setup needed for playing a classical 1970s chess program CHEKMO-II on modern computers.
+It will be useful for those who are interested in computer chess history.
+CHEKMO-II was created for [PDP-8](https://en.wikipedia.org/wiki/PDP-8) computers by Digital Equipment Corporation instructor John E. Comeauin. For detailed learning of programs's internal structure, see [original PAL-8 source code](http://pop.aconit.org/Programs/StandAlone/chekmo.lst), written in PAL-8, PDP-8 assembly.
+
+### Running in simulator
+
+You will need to install [SIMH](http://simh.trailing-edge.com), an archaic computers simulator.
+
+On Mac, this can be done using brew:
+
+``
+brew install simh
+``
+
+On Ubuntu and other Debian-based Linux distributions, you can use apt:
+
+``
+sudo apt-get install simh
+``
+
+To run CHEKMO-II use the ``run`` script at the root of this repository:
+
+``
+./run
+``
+
+![](img/simh.png?raw=true)
+
+To get to know the program's command set and other useful information, see the original user manual [chekmo.pdf](chekmo.pdf?raw=true).
+
+### Using the UCI adapter
+
+You can use any chess graphic interface program, that supports [Universal Chess Interface](https://chessprogramming.wikispaces.com/UCI). Set `chekmo-uci.py` as the chess engine executable to do that.
+
+For example, in [XBoard](https://www.gnu.org/software/xboard/),
+set up the engine with Menu – Engine – Edit Engine List. Append the following string to the list:
+
+``
+"CHEKMO-2" -fcp "./chekmo-uci.py" -fd "/path/to/this/repo/" -fUCI 
+``
+
+Now you can play with CHEKMO-II via graphic interface.
+
+![](img/xboard.png?raw=true)
+
+Using UCI clients, you can face CHEKMO-II off against modern engines.
+
+Example of CHEKMO-II, playing the modern Fairy-Max engine:
+
+![](img/vs.png?raw=true)
---
+++ chekmo-uci.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+#
+# PDP-8's CHEKMO-II chess engine UCI adapter
+#
+# (C) 2016 Oleg Kalachev <okalachev@gmail.com>
+#
+# http://github.com/okalachev/chekmo
+#
+
+from subprocess import Popen, PIPE
+import pty
+import os
+import sys
+import atexit
+
+
+pdir = os.path.dirname(os.path.realpath(__file__))
+
+
+def which(pgm):
+    path = os.getenv('PATH') + os.path.pathsep + '/usr/local/bin'
+    for p in path.split(os.path.pathsep):
+        p = os.path.join(p, pgm)
+        if os.path.exists(p) and os.access(p, os.X_OK):
+            return p
+
+
+def dump(s):
+    with open(pdir + '/dump.txt', 'a') as d:
+        d.write(s + '\n')
+
+
+def flush():
+    sys.stdout.flush()
+
+
+def wait_prompt():
+    parsed_side = None
+    while True:
+        x = chekmo.stdout.read(1)
+        if x == '?':
+            break
+        if x in ['W', 'B']:
+            parsed_side = x
+    chekmo.stdout.read(1)  # skip space char
+    return parsed_side
+
+
+def send(s):
+    pin.write(s + '\r')
+    pin.flush()
+
+
+def read_line():
+    return chekmo.stdout.readline()
+
+
+master, slave = pty.openpty()  # create a pseudoterminal to control pdp8 simulator
+
+chekmo = Popen([which('pdp8'), pdir + '/simh-script'], stdout=PIPE, stdin=slave, close_fds=True, shell=False)
+pin = os.fdopen(master, 'w')
+atexit.register(lambda: chekmo.kill())  # kill pdp8 simulator process on exit
+
+print 'CHEKMO-II UCI ADAPTER'
+#dump('run with ' + ' '.join(sys.argv))
+
+CASTLING = {
+    'W': {'O-O-O': 'e1c1', 'O-O': 'e1g1'},
+    'B': {'O-O-O': 'e8c8', 'O-O': 'e8g8'}
+}
+
+while True:
+    command = raw_input()
+    #dump(command)
+    words = command.split()
+    cleaned = ' '.join(words)
+    if len(words) == 0:
+        continue
+    cmd = words[0]
+
+    # Process a UCI command
+    if cmd == 'uci':
+        print 'id name CHEKMO-II'
+        print 'id author John E. Comeauin'
+        print 'option name Blitz mode type check default false'
+        print 'uciok'
+        flush()
+    elif cmd == 'isready':
+        print 'readyok'
+        flush()
+    elif cmd == 'ucinewgame':
+        wait_prompt()
+        send('re')
+    elif cmd == 'position' and words[1] == 'startpos' and len(words) == 2:
+        wait_prompt()
+        send('re')
+    elif cmd == 'position' and words[1] == 'startpos' and words[2] == 'moves':
+        moves = words[3:]
+        wait_prompt()
+        send('re')  # reset the board
+        for move in moves:
+            wait_prompt()
+            send(move)  # make moves
+    elif cmd == 'go':
+        side = wait_prompt()
+        send('mv')
+        read_line()
+        move = read_line().strip()
+        if CASTLING[side].get(move):
+            move = CASTLING[side].get(move)
+        else:
+            move = move.replace('-', '').replace('=', '').replace('+', '').replace(':', '').strip().lower()
+        print 'bestmove ' + move
+        flush()
+    elif cleaned == 'setoption name Blitz mode value true':
+        wait_prompt()
+        send('bm')
+    elif cleaned == 'setoption name Blitz mode value false':
+        wait_prompt()
+        send('tm')
+    elif cmd == 'quit':
+        sys.exit()
+    else:
+        print 'Unknown command: ' + command
